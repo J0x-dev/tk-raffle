27527272b8fe0acc0e842a09eb8fd24a9249670a
Mark Julius <marktestzxc@gmail.com>
2025-07-10 08:33:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/purchase-form.tsx
+++ src/components/purchase-form.tsx
@@ -100,8 +100,8 @@ export function PurchaseForm() {
   return (
     <Card className="w-full max-w-4xl bg-transparent !border-none shadow-none">
       <CardHeader>
-        <CardTitle className="font-headline font-bold text-[28px] text-left text-[#8a2a2b]">Form</CardTitle>
-        <CardDescription className="text-[#8a2a2b]">change to form test</CardDescription>
+        <CardTitle className="font-headline font-bold text-[28px] text-left text-[#8a2a2b]">Join Tapa King Royal Escape 38th Anniversary Vacation Raffle</CardTitle>
+        <CardDescription className="text-[#8a2a2b]">Enjoy your Tapa Favorites and get a chance to win your dream vacation!</CardDescription>
       </CardHeader>
       <CardContent>
         <Form {...form}>
@@ -399,3 +399,5 @@ export function PurchaseForm() {
     </Card>
   );
 }
+
+    
