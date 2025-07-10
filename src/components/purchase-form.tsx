48cdca7248e4d9eaaabbb23169fa7dc29308f055
Mark Julius <marktestzxc@gmail.com>
2025-07-10 07:10:07 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardTitle classNa

--- src/components/purchase-form.tsx
+++ src/components/purchase-form.tsx
@@ -81,7 +81,7 @@ export function PurchaseForm() {
   return (
     <Card className="w-full max-w-4xl bg-[#ede5d2] !border-none">
       <CardHeader>
-        <CardTitle className="font-headline text-[32px] text-center text-[#8a2a2b]">Sample Form</CardTitle>
+        <CardTitle className="font-headline font-bold text-[26px] text-left text-[#8a2a2b]">Sample Form</CardTitle>
       </CardHeader>
       <CardContent>
         <Form {...form}>
