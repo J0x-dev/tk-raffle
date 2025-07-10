1e9b0ac192db93ade5d8c3df76d2d769e58821ba
Mark Julius <marktestzxc@gmail.com>
2025-07-10 07:13:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <PurchaseForm>
    
    wi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { PurchaseForm } from '@/components/purchase-form';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8 md:p-12">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center">
       <PurchaseForm />
     </main>
   );
