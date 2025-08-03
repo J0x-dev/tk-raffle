0b18455bc8684d57c85a09bba9eaf3a3bb53b2d6
markmella0x <markmella0x@gmail.com>
2025-08-03 15:51:27 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import FaqContent from '@/components/faq-content';
 import InPartnership from '@/components/in-partnership';
 import TravelDestinations from '@/components/travel-destinations';
 import Image from 'next/image';
-import Link from 'next/link';
 import { blurSamalIsland } from '@/data/blurData';
 import FooterContent from '@/components/footer-content';
+import JumpLink from '@/components/jump-link';
 
 export default function Home() {
   return (
@@ -29,24 +29,7 @@ export default function Home() {
           blurDataURL={blurSamalIsland}
         />
 
-        <div className="absolute -bottom-16 left-1/2 z-20 w-[calc(100%-70px)] -translate-x-1/2 rounded-2xl bg-white p-3 text-warm-red shadow-lg sm:max-w-3xl sm:p-4 md:max-w-3xl md:px-6">
-          <h2 className="text-center text-base font-extrabold sm:text-3xl">
-            Discover the Philippines
-          </h2>
-          <p className="mt-2 text-center text-xs text-warm-orange sm:text-xl">
-            In celebration of our 38th Anniversary, get a chance to experience
-            an escape with Tapa King's{' '}
-            <span className="italic">
-              Discover Philippines: Travel Raffle Promo!
-            </span>
-          </p>
-          <Link
-            href="/raffle-form"
-            className="duration-800 mx-auto mt-3 block w-fit cursor-pointer rounded-full bg-warm-red px-6 py-1 text-base font-extrabold text-white sm:mt-5 sm:px-8 sm:py-2 sm:text-2xl"
-          >
-            Join Now
-          </Link>
-        </div>
+        <JumpLink />
       </div>
 
       <div className="relative w-full pt-24 sm:pt-32">
---
+++ src/components/jump-link.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function JumpLink() {
+  return (
+    <div className="absolute -bottom-16 left-1/2 z-20 w-[calc(100%-70px)] -translate-x-1/2 rounded-2xl bg-white p-3 text-warm-red shadow-lg sm:max-w-3xl sm:p-4 md:max-w-3xl md:px-6">
+      <h2 className="text-center text-base font-extrabold sm:text-3xl">
+        Discover the Philippines
+      </h2>
+      <p className="mt-2 text-center text-xs text-warm-orange sm:text-xl">
+        In celebration of our 38th Anniversary, get a chance to experience an
+        escape with Tapa King's{' '}
+        <span className="italic">
+          Discover Philippines: Travel Raffle Promo!
+        </span>
+      </p>
+      <Link
+        href="/raffle-form"
+        className="duration-800 mx-auto mt-3 block w-fit cursor-pointer rounded-full bg-warm-red px-6 py-1 text-base font-extrabold text-white sm:mt-5 sm:px-8 sm:py-2 sm:text-2xl"
+      >
+        Join Now
+      </Link>
+    </div>
+  );
+}
