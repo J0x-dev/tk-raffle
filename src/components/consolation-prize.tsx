e56f690602291a46325d40a8a86609b5c9016832
J0x-dev <markmella0x@gmail.com>
2025-08-05 07:22:12 +0000

    123

--- src/components/consolation-prize.tsx
+++ src/components/consolation-prize.tsx
@@ -1,8 +1,8 @@
 export default function consolationPrize() {
   return (
-    <div className="mx-auto mt-12 flex min-h-20 w-[calc(100%-40px)] max-w-[450px] gap-2 rounded-2xl bg-warm-red p-2">
-      <div className="flex h-[65px] w-[70px] flex-col items-center justify-center rounded-xl border-2 border-white pt-2 sm:h-[85px] sm:w-[90px]">
-        <h2 className="text-[45px] font-extrabold leading-[32px] -tracking-wide text-white sm:text-[58px] sm:leading-[45px]">
+    <div className="mx-auto mt-12 flex min-h-20 w-[310px] gap-2 rounded-2xl bg-warm-red p-2 sm:w-[450px]">
+      <div className="flex h-[65px] w-[70px] flex-col items-center justify-center rounded-xl border-2 border-white pt-2 sm:h-[85px] sm:w-[90px] sm:pt-3">
+        <h2 className="text-[45px] font-extrabold leading-[32px] -tracking-wide text-white sm:text-[58px] sm:leading-[38px]">
           38
         </h2>
         <h3 className="text-sm leading-[16px] text-white sm:text-base">
